21347fc09bb276b2d89bfacb0731d111def3d5d4
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-12-23 03:11:08 -0300

    fix: build.sh

--- build.sh
+++ build.sh
@@ -1,47 +1,35 @@
 #!/bin/bash
 
-# Output directory
 OUTPUT_DIR="bin"
 
-# Binary name
 BINARY_NAME="kubectl-monitor"
 
-# Create the output directory if it doesn't exist
 if [ ! -d "$OUTPUT_DIR" ]; then
-    echo "Creating directory $OUTPUT_DIR..."
-    mkdir -p "$OUTPUT_DIR"
+  echo "Creating directory $OUTPUT_DIR..."
+  mkdir -p "$OUTPUT_DIR"
 fi
 
-# Build for the local platform
-echo "Building binary for the local platform..."
-GOOS=$(go env GOOS)
-GOARCH=$(go env GOARCH)
-OUTPUT_FILE="$OUTPUT_DIR/$BINARY_NAME-$GOOS-$GOARCH"
-
-go build -o "$OUTPUT_FILE" main.go
-
-if [ $? -eq 0 ]; then
-    echo "Binary created: $OUTPUT_FILE"
-else
-    echo "Failed to build binary for the local platform."
-    exit 1
-fi
-
-# Optional: Build for other platforms
-PLATFORMS=("linux/amd64" "linux/arm64" "windows/amd64")
+PLATFORMS=(
+  "darwin/amd64" # macOS 64-bit
+  "darwin/arm64" # macOS (M1/M2)
+  "linux/amd64"  # Linux 64-bit
+  "linux/arm64"  # Linux ARM 64-bit
+)
 
 for PLATFORM in "${PLATFORMS[@]}"; do
-    IFS="/" read -r GOOS GOARCH <<< "$PLATFORM"
-    OUTPUT_FILE="$OUTPUT_DIR/$BINARY_NAME-$GOOS-$GOARCH"
+  IFS="/" read -r GOOS GOARCH <<<"$PLATFORM"
+  OUTPUT_FILE="$OUTPUT_DIR/$BINARY_NAME-$GOOS-$GOARCH"
 
-    echo "Building binary for $GOOS/$GOARCH..."
-    GOOS=$GOOS GOARCH=$GOARCH go build -o "$OUTPUT_FILE" main.go
+  echo "Building binary for $GOOS/$GOARCH..."
+  GOOS=$GOOS GOARCH=$GOARCH go build -o "$OUTPUT_FILE" main.go
 
-    if [ $? -eq 0 ]; then
-        echo "Binary created: $OUTPUT_FILE"
-    else
-        echo "Failed to build binary for $GOOS/$GOARCH."
-    fi
+  if [ $? -eq 0 ]; then
+    echo "Binary created: $OUTPUT_FILE"
+  else
+    echo "Failed to build binary for $GOOS/$GOARCH."
+    exit 1
+  fi
+
 done
 
-echo "All binaries have been created in the $OUTPUT_DIR folder."
+echo "All required binaries have been created in the $OUTPUT_DIR folder."
